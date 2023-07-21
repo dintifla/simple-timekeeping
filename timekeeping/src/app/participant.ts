adc63ab35e76cc0c33ab893d1e3fd8d06d63a00c
Flavio D'Intino <flavio.dintino@gmail.com>
2023-07-21 16:06:53 +0200

    refactor: move angular to main project

--- timekeeping/src/app/participant.ts
+++
@@ -1,8 +0,0 @@
-export interface Participant {
-  numberPlate: number;
-  name: string;
-  isSpare?: boolean;
-  category: string;
-
-  time?: Date | string;
-}
--- timekeeping/src/app/timing.ts
+++
@@ -1,7 +0,0 @@
-export interface Timing {
-  numberPlate: number;
-  category: string;
-  name: string;
-  startTime: string;
-  finishTime: string;
-}
